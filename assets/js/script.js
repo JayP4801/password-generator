772eafed3f5ad5a3eccc8af2f46530b4765062d1
JayP4801 <jpham4801@gmail.com>
2022-03-07 12:55:16 -0500

    complete generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,91 @@
 // Assignment code here 
 // *********************
 
+// function to generate password after button click
 function generatePassword(){
   var password = "";
   var passwordLength = 0;
-  var passwordLengthPrompt = window.prompt("Please choose password length between 8 - 128 characters long.");
+  var passwordLengthPrompt = window.prompt('Please choose password length between 8 - 128 characters long.');
   var characters = "";
 
-  if (passwordLengthPrompt > 7 && passwordLengthPrompt < 128){
+  if (passwordLengthPrompt >= 8 && passwordLengthPrompt <= 128){
     passwordLength = passwordLengthPrompt;
   } else{
     window.alert("Please select between 8 - 128 characters");
     return generatePassword();
   };
 
+  // ask user if password should include lowercase letters
   const lower = "abcdefghijklmnopqrstuvwxyz";
-  var lowercasePrompt = window.prompt("Do you want to include lowercase letters?");
+  var lowercasePrompt = window.prompt('Do you want to include lowercase letters? Type "Y" for YES or "N" for NO');
   switch(lowercasePrompt){
-    case "YES":
-    case "yes":
+    case "Y":
+    case "y":
       characters += lower;
       break;
-    case "NO":
-    case "no":
+    case "N":
+    case "n":
       break;
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+    generatePassword();
   };
 
+  // ask user if password should include uppercase letters
   const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var uppercasePrompt = window.prompt("Do you want to include uppercase letters?");
+  var uppercasePrompt = window.prompt('Do you want to include uppercase letters? Type "Y" for YES or "N" for NO');
   switch(uppercasePrompt){
-    case "YES":
-    case "yes":
+    case "Y":
+    case "y":
       characters += upper;
       break;
-    case "NO":
-    case "no":
+    case "N":
+    case "n":
       break;
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+      generatePassword();
   };
 
+  // ask user if password should include numbers
   const number = "1234567890";
-  var numberPrompt = window.prompt("Do you want to include numbers?");
-  numberPrompt = partInt(numberPrompt);
+  var numberPrompt = window.prompt('Do you want to include numbers? Type "Y" for YES or "N" for NO');
+  numberPrompt = numberPrompt;
   switch(numberPrompt){
-    case "YES":
-    case "yes":
+    case "Y":
+    case "y":
       characters += number;
       break;
-    case "NO":
-    case "no":
+    case "N":
+    case "n":
       break;
+    default:
+      window.alert('You did not pick a valid option. Try again.');
+      generatePassword();
   };
 
+  // ask user if password should include special characters
   const special = "!@#$%^&*_-+=";
-  var specialCharacterPrompt = window.prompt("Do you want to include special characters?");
+  var specialCharacterPrompt = window.prompt('Do you want to include special characters? Type "Y" for YES or "N" for NO');
   switch(specialCharacterPrompt){
-    case "YES":
-    case "yes":
+    case "Y":
+    case "y":
       characters += special;
       break;
-    case "NO":
-    case "no":
+    case "N":
+    case "n":
       break;
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+      generatePassword();
   };
 
-  return passwordLength;
-
-  
+  // for loop to randomize password character order
+  for (var i = 0; i < passwordLength; i++){
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return password;
 };
-// prompt for length of the password
-
-// confirm to include lowercase letters
-
-// confirm to include uppercase letters
-
-// confirm to include numbers
-
-// confirm to include special characters
-
-// Get references to the #generate element
 
 var generateBtn = document.querySelector("#generate");
 
